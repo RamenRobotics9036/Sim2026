0b0074189bbdf08078af66695cf737812c96eb84
Ido <idoben2@hotmail.com>
2026-01-17 19:17:18 -0800

    Add visualization for wheels and cameras

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,9 +6,13 @@ package frc.robot;
 
 import com.ctre.phoenix6.HootAutoReplay;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.generated.TunerConstants;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -87,14 +91,35 @@ public class Robot extends TimedRobot {
     @Override
     public void simulationPeriodic() {
         // Update vision simulation with current robot pose
-        m_vision.simulationPeriodic(m_robotContainer.drivetrain.getState().Pose);
+        var driveState = m_robotContainer.drivetrain.getState();
+        m_vision.simulationPeriodic(driveState.Pose);
 
-        // Visualize estimated pose on field
+        // Visualize estimated pose and module poses on field
         var debugField = m_vision.getSimDebugField();
         if (debugField != null) {
-            debugField.getObject("EstimatedRobot").setPose(
-                m_robotContainer.drivetrain.getState().Pose
+            debugField.getObject("EstimatedRobot").setPose(driveState.Pose);
+            debugField.getObject("EstimatedRobotModules").setPoses(getModulePoses(driveState));
+        }
+    }
+
+    /**
+     * Get the Pose2d of each swerve module based on the current robot pose and module states.
+     */
+    private Pose2d[] getModulePoses(com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState driveState) {
+        // Module locations relative to robot center (from TunerConstants)
+        Translation2d[] moduleLocations = {
+            new Translation2d(TunerConstants.FrontLeft.LocationX, TunerConstants.FrontLeft.LocationY),
+            new Translation2d(TunerConstants.FrontRight.LocationX, TunerConstants.FrontRight.LocationY),
+            new Translation2d(TunerConstants.BackLeft.LocationX, TunerConstants.BackLeft.LocationY),
+            new Translation2d(TunerConstants.BackRight.LocationX, TunerConstants.BackRight.LocationY)
+        };
+
+        Pose2d[] modulePoses = new Pose2d[4];
+        for (int i = 0; i < 4; i++) {
+            modulePoses[i] = driveState.Pose.transformBy(
+                new Transform2d(moduleLocations[i], driveState.ModuleStates[i].angle)
             );
         }
+        return modulePoses;
     }
 }
